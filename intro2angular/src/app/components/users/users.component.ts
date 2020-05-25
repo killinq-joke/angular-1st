d24a5fac98fb3794862334aaaefc465fff3c0d8c
killinq-joke <zakadmaster@gmail.com>
2020-05-25 15:24:37 +0200

    Addition of button & form

--- intro2angular/src/app/components/users/users.component.ts
+++ intro2angular/src/app/components/users/users.component.ts
@@ -3,13 +3,34 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
 })
 export class UsersComponent implements OnInit {
+  name: string;
+  age: number;
+  email: string;
+  hobby: hobby;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.name = 'zak';
+    this.age = 20;
+    this.email = 'zakadmaster@gmail.com';
+    this.hobby = {
+      hobby1: 'sport',
+      hobby2: 'games',
+      hobby3: 'code',
+    };
   }
 
+  ngOnInit() {}
+
+  onClick() {
+    console.log('STOP PRESSING ME');
+  }
+}
+
+interface hobby {
+  hobby1: string;
+  hobby2: string;
+  hobby3: string;
 }
